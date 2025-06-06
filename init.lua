29c35fc893a60ac2a5c965da7a83f16c2c8fdeec
Anartz Martin <anartzmr@gmail.com>
2025-06-06 11:28:26 +0200

    Config: removed 'black' and 'isort' for python and added 'ruff' instead.

--- init.lua
+++ init.lua
@@ -787,10 +787,18 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "black" },
+        -- python = { "isort", "black" },
+        python = {
+          -- To fix auto-fixable lint errors.
+          'ruff_fix',
+          -- To run the Ruff formatter.
+          'ruff_format',
+          -- To organize the imports.
+          'ruff_organize_imports',
+        },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
